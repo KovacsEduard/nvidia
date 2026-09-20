e47114af24be81e5f7786cbb6e3a5b916819f25c
Kovacs Eduard <kovaceduard2@gmail.com>
2026-09-20 18:18:30 +0200

    building

---
+++ toggleInfo.js
@@ -0,0 +1,8 @@
+        function toggleInfo(panelId) {
+            var panel = document.getElementById(panelId);
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        }
